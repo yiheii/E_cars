1b078890fadd4ac9b49c9f2893b2676a17d3f82a
Eddie <zmaa0011@student.monash.edu>
2019-09-15 20:50:28 +1000

    Initial commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(E_cars.Startup))]
+namespace E_cars
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
